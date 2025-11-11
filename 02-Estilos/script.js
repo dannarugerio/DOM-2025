a91895abd3012a9866bb0dea0cae529a8dae4b31
dannarugerio <182258178+dannarugerio@users.noreply.github.com>
2025-11-11 15:40:50 -0600

    Estilos con Javascript

---
+++ 02-Estilos/script.js
@@ -0,0 +1,58 @@
+/* 
+
+Selecionar el h1 a través de su clase
+
+/* 
+
+const titulo = document.querySelector(".titulo");
+
+/* 
+
+Modo seleccionador
+
+/* 
+
+console.log(titulo);
+
+/* 
+
+Objeto style
+
+Acceder a este objeto usando la  notación de punto
+  nodo.style
+Resultado es CSS Style Declaration. Es decir una lista que representa todas las propiedades de estilo del elemento.
+
+Las Propedades cambian del formato kebab-case a camelCase
+
+Unicamente muestra el valor de los estilos en linea declarados directamente en el elemento HTML
+*/ 
+
+/* 
+
+console.log(titulo.style)
+
+*/ 
+
+/*
+Para acceder a las propiedaddes de estilo usamos la notación de (.)
+nodo.style.nombrePropiedad
+
+*/
+
+/*
+console.log(`El color del titulo es: ` + titulo.style.color)
+
+*/
+
+/*
+Asignar un valor a la propiedad de esilo del nodo seleccionado.
+
+  node.style.color=valor
+
+*/
+
+titulo.style.color="black";
+
+titulo.style.backgroundColor="rose";
+
+titulo.style.fontSize="64px"
