31f3295a25c718aa9cd0ef43d5cf582efb3ad422
dannarugerio <182258178+dannarugerio@users.noreply.github.com>
2025-11-25 15:32:13 -0600

    Segunda sesión del DOM

--- 02-Estilos/script.js
+++ 02-Estilos/script.js
@@ -2,7 +2,7 @@
 
 Selecionar el h1 a través de su clase
 
-/* 
+*/
 
 const titulo = document.querySelector(".titulo");
 
@@ -10,7 +10,7 @@ const titulo = document.querySelector(".titulo");
 
 Modo seleccionador
 
-/* 
+*/
 
 console.log(titulo);
 
@@ -27,11 +27,9 @@ Las Propedades cambian del formato kebab-case a camelCase
 Unicamente muestra el valor de los estilos en linea declarados directamente en el elemento HTML
 */ 
 
-/* 
 
 console.log(titulo.style)
 
-*/ 
 
 /*
 Para acceder a las propiedaddes de estilo usamos la notación de (.)
@@ -39,10 +37,7 @@ nodo.style.nombrePropiedad
 
 */
 
-/*
 console.log(`El color del titulo es: ` + titulo.style.color)
-
-*/
 
 /*
 Asignar un valor a la propiedad de esilo del nodo seleccionado.
@@ -56,3 +51,52 @@ titulo.style.color="black";
 titulo.style.backgroundColor="rose";
 
 titulo.style.fontSize="64px"
+
+/* 
+
+Método setProperty()
+
+Asignamos una propiedad de estilo a un elemento seleccionado.
+
+Sintaxis:
+
+elemento.style.setProperty(nombrePropiedad, valor, important);
+
+  - Más flexible, y por lo tanto más usado.
+  - Las propiedaddes se escriben en kebab-kase
+  -El parametro "important" es opcional.
+
+*/
+
+titulo.style.setProperty("color", "dark")
+titulo.style.setProperty("background-color", "green", "important")
+
+/* 
+
+eliminar calores de propiedades del nodo seleccionado nodo,style.nombrePropiedad 
+
+en esta sintaxis la propiedad de CSS va en formato camelCase
+
+*/
+
+//quita estilos
+/* titulo.style.color= "";
+titulo.style.backgroundColor= "";
+ */
+/* 
+
+Metodo removeProperty()
+
+Esto elimina la propiedad de estilo
+
+Sintaxis:
+
+elemento.styleremoveProperty(nombre-propiedad)
+
+El nombre de la propiedad es en formato keba-case
+
+*/
+
+titulo.style.removeProperty("color");
+titulo.style.removeProperty("background-color");
+titulo.style.removeProperty("font-size");
